4d9ba3e1f08b7625759abcadc79fd3dd89706439
s-hirose <shunichi_hirose@pj100.biz>
2022-09-12 16:51:30 +0900

    :+1: many to one用のモデルを作成

--- relationships/admin.py
+++ relationships/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Place, Restaurant, Reporter, Article
 
 # Register your models here.
+admin.site.register(Place)
+admin.site.register(Restaurant)
+admin.site.register(Reporter)
+admin.site.register(Article)
--- relationships/models.py
+++ relationships/models.py
@@ -5,8 +5,12 @@ class Place(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=80)
 
+    class Meta:
+        ordering = ["name"]
+        db_table = "Place"
+
     def __str__(self):
-        return "%s the place" % self.name
+        return self.name
 
 
 class Restaurant(models.Model):
@@ -18,13 +22,35 @@ class Restaurant(models.Model):
     serves_hot_dogs = models.BooleanField(default=False)
     serves_pizza = models.BooleanField(default=False)
 
-    def __str__(self):
-        return "%s the restaurant" % self.place.name
-
-
-class Waiter(models.Model):
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    class Meta:
+        ordering = ["place"]
+        db_table = "Restaurant"
 
     def __str__(self):
-        return "%s the waiter at %s" % (self.name, self.restaurant)
+        return self.name
+
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name}" + " " + f"{self.last_name}"
+
+    class Meta:
+        db_table = "Reporter"
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
+
+    # headlineの順に並び替える
+    class Meta:
+        db_table = "Article"
+        ordering = ["headline"]
