5ab7be1e80511d59d1d94ffadab8fdd273d437e5
s-hirose <shunichi_hirose@pj100.biz>
2022-09-13 08:47:25 +0900

    :+1: add serializers.py and urls.py

--- relationships/views.py
+++ relationships/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView
+
+from relationships.serializers import PlaceSerializer
+from .models import Place
 
 # Create your views here.
+class PlaceAPIView(RetrieveAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
