c9df789bc16f7b88f69b5d43733f27e448a5023b
shun198 <shunhiroseluvmri@gmail.com>
2022-09-14 20:55:52 +0900

    :+1: add urls

--- relationships/models.py
+++ relationships/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Place(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=80)
 
