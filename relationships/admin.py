95a0aa9dd22c03955cb10bba9cb6ca6b13df923a
s-hirose <shunichi_hirose@pj100.biz>
2022-09-16 08:27:01 +0900

    :+1: migrationsを削除

--- relationships/admin.py
+++ relationships/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Place, Restaurant, Reporter, Article, Publication, Fruits
+from .models import Place  # , Restaurant, Reporter, Article, Publication, Fruits
 
 # Register your models here.
 admin.site.register(Place)
-admin.site.register(Restaurant)
-admin.site.register(Reporter)
-admin.site.register(Article)
-admin.site.register(Publication)
-admin.site.register(Fruits)
+# admin.site.register(Restaurant)
+# admin.site.register(Reporter)
+# admin.site.register(Article)
+# admin.site.register(Publication)
+# admin.site.register(Fruits)
