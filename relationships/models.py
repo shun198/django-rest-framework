d45a0f750b643e4b1f60c1c06f16fc1ec840fc67
s-hirose <shunichi_hirose@pj100.biz>
2022-09-26 16:52:20 +0900

    :+1: 修正

--- relationships/models.py
+++ relationships/models.py
@@ -1,23 +1,26 @@
 from django.db import models
 
+
 class Book(models.Model):
     name = models.CharField(max_length=50)
     date = models.DateField()
+
     class Meta:
-        ordering = ["name"]
+        ordering = ["name", "date"]
         db_table = "Book"
 
     def __str__(self):
         return self.name
+
+
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
-    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ["name"]
+        ordering = ["name", "age", "book"]
         db_table = "Customer"
 
     def __str__(self):
         return self.name
-    
--- relationships/views.py
+++ relationships/views.py
@@ -1,15 +1,21 @@
 from rest_framework import viewsets
-
-from .serializers import CustomerSerializer,BookSerializer
-from .models import Customer,Book
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.http import (
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseNotFound,
+    JsonResponse,
+)
+from .serializers import CustomerSerializer, BookSerializer
+from .models import Customer, Book
 
 # Create your views here.
 class CustomerViewSets(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
 
-    
+
 class BookViewSets(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    
