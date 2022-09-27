278328eeb09421af4b3b105b7a9425bc64696d93
s-hirose <shunichi_hirose@pj100.biz>
2022-09-27 11:50:40 +0900

    :+1: viewsetに一通りのHTTPメソッドを追加

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
-urlpatterns = [path("hello-view/", views.HelloApiView.as_view())]
+router = DefaultRouter()
+# base_name is for retrieving url
+router.register("hello-viewset",views.HelloViewSet,basename="hello-viewset")
+
+urlpatterns = [
+    path("hello-view/", views.HelloApiView.as_view()),
+    path("",include(router.urls))
+]
