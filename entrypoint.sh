951707e1ebc110a48ac6f3f2833c0ea7dac60adf
shun198 <shunhiroseluvmri@gmail.com>
2022-10-17 20:41:44 +0900

    👍 poetryを追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-    python manage.py makemigrations --noinput
-    python manage.py migrate --noinput
-    python manage.py collectstatic --noinput
+    poetry run python manage.py makemigrations --noinput
+    poetry run python manage.py migrate --noinput
+    poetry run python manage.py collectstatic --noinput
 if [$DEBUG == True]; then
-    python manage.py runserver 0.0.0.0:8000
+    poetry run ython manage.py runserver 0.0.0.0:8000
 else
-    gunicorn study.wsgi:application --bind 0.0.0.0:8000
+    poetry run gunicorn study.wsgi:application --bind 0.0.0.0:8000
 fi
