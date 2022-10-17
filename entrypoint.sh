c80586cc8fd67570e6f5cdde2b94b57e1bee79d7
shun198 <shunhiroseluvmri@gmail.com>
2022-10-17 19:11:57 +0900

    👍 entrypoint.shを追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
+    python manage.py makemigrations --noinput
+    python manage.py migrate --noinput
+    python manage.py collectstatic --noinput
+if [$DEBUG == True]; then
+    python manage.py runserver 0.0.0.0:8000
+else
+    gunicorn study.wsgi:application --bind 0.0.0.0:8000
+fi
--- study/settings.py
+++ study/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # DEBUGを.envから取得
-DEBUG = os.environ.get("DEBUG")
+DEBUG = bool(int(os.environ.get("DEBUG",0)))
 
 # ALLOWED_HOSTSを.envから取得
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
